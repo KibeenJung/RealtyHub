27331202e4c64e058d0c710c915f5a145462b980
정기빈 <hellome3051@gmail.com>
2024-03-06 16:38:20 +0900

    chapter 8. complted

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,80 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div v-if="room_info_window.is_open == true" class="black-background">
+    <div class="white-background">
+      <img :src="room_info_window.room.image" class="room-image" />
+      <h4> {{ room_info_window.room.title }} </h4>
+      <p> {{ distinguish_with_commas(room_info_window.room.price) }} 원 </p>
+      <p> {{ room_info_window.room.content }} </p>
+      <button @click="room_info_window.is_open = false"> 나가기 </button>
+    </div>
+  </div>
+
+  <Logo></Logo>
+  <Menu></Menu>
+
+  <Discount></Discount>
+
+  <div v-for="room in rooms" :key="room">
+    <img :src="room.image" class="room-image" />
+    <h4 @click="room_info_window.is_open = true; room_info_window.room = room"> {{ room.title }} </h4>
+    <p> {{ distinguish_with_commas(room.price) }} 원 </p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import rooms from "./assets/rooms.js";
+import Discount from './discount.vue'
+import Logo from './logo.vue'
+import Menu from './menu.vue'
 
 export default {
   name: 'App',
+  data() {
+    return {
+      rooms: rooms,
+      room_info_window: { is_open: false, room: null },
+
+    }
+  },
+  methods: {
+    distinguish_with_commas: function (number) {
+      return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+    }
+  },
+  created() {
+  },
   components: {
-    HelloWorld
+    Discount: Discount,
+    Logo: Logo,
+    Menu: Menu,
   }
 }
 </script>
 
 <style>
+body {
+  margin: 0
+}
+
+div {
+  box-sizing: border-box;
+}
+
+.black-background {
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  padding: 20px;
+}
+
+.white-background {
+  width: 100%;
+  background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -22,5 +82,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.room-image {
+  width: 50%;
+  margin-top: 40px;
 }
 </style>
---
+++ src/menu.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="menu">
+        <menu_comp v-for="menu_name in menus" :key="menu_name"> {{ menu_name }}</menu_comp>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Menu",
+    data() {
+        return {
+            menus: ['Home', 'Shop', 'About'],
+        }
+    }
+}
+</script>
+
+<style>
+.menu {
+    background: darkslateblue;
+    padding: 15px;
+    border-radius: 5px;
+}
+
+.menu menu_comp {
+    color: white;
+    padding: 10px;
+}
+</style>
